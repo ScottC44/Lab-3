f3d69b418eaf5d0d8a230a2933831c31cb4c9687
Scott L Cromling <slcromling@ncat.edu>
2020-02-12 17:44:45 -0500

    fixed my project now it properly reads and outputs

--- src/PersonInfo.java
+++ src/PersonInfo.java
@@ -6,61 +6,15 @@ import java.util.Scanner;
 
 public class PersonInfo {
 
-    private static String firstName;
-    private static String lastName;
-    private static String gender;
-    private static int age;
-    private static String phoneNo;
-    private static String email;
-
-    public PersonInfo(String firstName, String lastName, String gender, int age, String phoneNo, String email) {
-
-        PersonInfo.firstName = firstName;
-        PersonInfo.lastName = lastName;
-        PersonInfo.gender = gender;
-        PersonInfo.age = age;
-        PersonInfo.phoneNo = phoneNo;
-        PersonInfo.email = email;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        PersonInfo.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        PersonInfo.lastName = lastName;
-    }
-    public String getGender() {
-        return gender;
-    }
-    public void setGender(String gender) {
-        PersonInfo.gender = gender;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        PersonInfo.age = age;
-    }
-    public void setPhoneNo(String phoneNo){
-        PersonInfo.phoneNo = phoneNo;
-    }
-    public String getPhoneNo(){
-        return phoneNo;
-    }
-    public void setEmail(String email){
-        PersonInfo.email = email;
-    }
-    public String getEmail(){
-        return email;
-    }
-
     public static void main(String[] args) {
-        ArrayList<PersonInfo> list = new ArrayList<>();
+         String firstName;
+         String lastName;
+         String gender;
+         int age;
+         String phoneNo;
+         String email;
+         String total;
+        ArrayList<String> list = new ArrayList<>();
         File file = new File("records.txt");
         try {
             Scanner scan = new Scanner(file);
@@ -86,7 +40,8 @@ public class PersonInfo {
                 if (email.matches("[A-Z][0-9].@")) {
                     email = email;
                 }
-                list.add(new PersonInfo(firstName, lastName, gender, age, phoneNo, email));
+                total = firstName +lastName + age +phoneNo +email;
+                list.add(total);
             }
             scan.close();
         } catch (FileNotFoundException e) {
@@ -94,9 +49,7 @@ public class PersonInfo {
         }
         System.out.format("%2s%20s%20s%20s%20s%20s ", "First Name", "Last Name", "Gender", "Age", "Phone no", "Email");
         for (int i = 0; i < list.size(); i++) {
-            System.out.format("%2s%21s%22s%22s%22s%30s ", list.get(i).getFirstName(), list.get(i).getLastName(),
-                    list.get(i).getGender(), list.get(i).getAge(), list.get(i).getPhoneNo(), list.get(i).getEmail());
-        }
+            System.out.println(list.get(i)); 		        }
     }
 }
 
